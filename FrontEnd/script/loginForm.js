5e843071ebde9c3ca6bc30d1731d39cd10e3810a
3mpty <aurelien.corre@outlook.fr>
2023-11-05 17:49:27 +0100

    Updated login function

--- FrontEnd/script/loginForm.js
+++ FrontEnd/script/loginForm.js
@@ -1,9 +1,7 @@
 // Fonction pour récupérer les données des inputs (login screen)
 const loginForm = document.querySelector("form");
 
-loginForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-
+const login = async () => {
     // Récupère les valeurs du nom d'utilisateur et du mot de passe
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -20,12 +18,20 @@ loginForm.addEventListener("submit", async (event) => {
     if (response.ok) {
         // Traitement de la réponse si la connexion est réussie
         const data = await response.json();
-        console.log(data);
+        const token = data.token;
+        console.log(token);
 
         // Redirection vers la page d'accueil si connexion OK
-        window.location.href = "../index.html";
+        if (token) {
+            window.location.href = "../index.html";
+        }
     } else {
         alert("Email ou mot de passe incorrect");
         console.error("La connexion a échoué.");
     }
+};
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    login();
 });
